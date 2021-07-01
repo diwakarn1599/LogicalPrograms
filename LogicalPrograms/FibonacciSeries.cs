f95534b90281140365fef3302e6631dd418e185e
Diwakar <diwakarn1599@gmail.com>
2021-07-01 14:09:17 +0530

    [Diwakar] Usecase 1 - Fibbonacci series completed

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        public static void GenerateFibonacciSeries()
+        {
+            int n1 = 0, n2 = 1, sum, limit;
+            Console.WriteLine("Enter How many numbers in the series should be generated");
+
+            //getting limit from the user
+            limit = Convert.ToInt32(Console.ReadLine());
+
+            if (limit == 1)
+            {
+                Console.WriteLine(n1);
+            }
+            else
+            {
+
+                Console.WriteLine(n1);
+                Console.WriteLine(n2);
+                limit -= 2;
+                //loop for generating fibbonacci series
+                while (limit > 0)
+                {
+                    sum = n1 + n2;
+                    Console.WriteLine(sum);
+                    n1 = n2;
+                    n2 = sum;
+                    limit -= 1;
+                }
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,26 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programs");
+
+            //Menu for list of programs
+            Console.WriteLine("Menu\n1.Fibbonacci Series\n2.Exit");
+
+            Console.WriteLine("Enter the option");
+
+            //Switch case
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    FibonacciSeries.GenerateFibonacciSeries();
+                    break;
+                case 2:
+                    Console.WriteLine("Exited");
+                    break;
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+
+            }
         }
     }
 }
